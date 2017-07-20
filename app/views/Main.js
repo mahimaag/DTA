0b7d08ae117b1f3942b615e8a0771583d1f5ac0a
lianboi <lbthomte@yahoo.com>
2017-07-20 10:13:52 +0530

    side panel

--- app/views/Main.js
+++ app/views/Main.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-
+import Card from '../components/NotificationCards';
 class Main extends Component {
-
-    render() {
-        return (
-            <div className="wrapper wrapper-content animated fadeInRight">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="text-center m-t-lg">
-                            <h1>
-                                Welcome in INSPINIA ReactJS Seed Project
-                            </h1>
-                            <small>
-                                It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
-                            </small>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="wrapper wrapper-content animated fadeInRight">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center m-t-lg">
+              <div className="col-md-4 pull-right">
+                <Card/>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 
 }
 
