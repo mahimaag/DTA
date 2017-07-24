175de42207e95304fd6ae8f4b07c10d7ccf654a9
guptakvgaurav <guptakvgaurav@gmail.com>
2017-07-24 14:35:38 +0530

    Calendar core component

--- app/views/Main.js
+++ app/views/Main.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import NotificationCards from '../components/NotificationCards';
+import DashboardCalendar from '../core/Calendar';
+import events from '../MockData/events';
 
 class Main extends Component {
     constructor(props){
@@ -12,9 +14,14 @@ class Main extends Component {
             <div className="wrapper wrapper-content animated fadeInRight">
                 <div className="row">
                     <div className="col-lg-12">
+                        <div>
+                            <div className="col-md-7">
+                                <DashboardCalendar events={events}/>
+                            </div>
+                        </div>
                         <div className="text-center m-t-lg">
                             <div className="col-md-4 pull-right">
-                                <NotificationCards/>
+                                <NotificationCards events={events}/>
                             </div>
                         </div>
                     </div>
