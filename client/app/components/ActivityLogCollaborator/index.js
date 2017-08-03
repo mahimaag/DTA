9c73afd1e5fc42a87e5c7cdb9445eef2c87622b6
Mahima Agrawal <mahi9.goyal@gmail.com>
2017-08-03 10:52:58 +0530

    Refactor ActivityLogCollaborator

--- client/app/components/ActivityLogCollaborator/index.js
+++ client/app/components/ActivityLogCollaborator/index.js
@@ -8,35 +8,16 @@ class ActivityLogCollaborator extends Component{
         super(props);
     }
 
-    onCollaboratorChange = (index, event) => {
-        let newArray = this.props.collaborators;
-        newArray[index] = event.target.value;
-        this.props.onCollabChange(newArray);
-    }
-
     render()
     {
         return (
             <div className="collab-div">
-                {this.props.editable ?
-                    <div>
-                        <span
-                            className="activity-collaborators">Collaborators:{this.props.collaborators.map((collaborator, index) => {
-                                return (<input type="text"
-                                               value={collaborator}
-                                               onChange={(value) => {this.onCollaboratorChange(index, value)}}/>)
-                            }
-                        )}
-                        </span>
-                    </div> :
-                    <span
-                        className="activity-collaborators">Collaborators:{this.props.collaborators.map((collaborator, index) => {
-                            return (<p key={index}>{collaborator}</p>)
-                        }
-                    )}
-                    </span>
-                }
-
+                <span
+                    className="activity-collaborators">Collaborators:{this.props.collaborators.map((collaborator, index) => {
+                        return (<p key={index}>{collaborator}</p>)
+                    }
+                )}
+                </span>
             </div>
         )
     }
