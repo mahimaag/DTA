680c4218b2d31dad3a87e3971c33be15d572083b
sourabh <=>
2017-08-11 11:50:55 +0530

    test cases written for card component

---
+++ client/test/app/core/CardContent.test.js
@@ -0,0 +1,23 @@
+/**
+ * Created by sourabh on 10/8/17.
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import  CardContent from "../../../app/Core/Card/CardContent"
+
+describe('Component :CardContent',()=>{
+    const Wrapper=mount(<CardContent style={{color:'red'}}>hello world</CardContent>);
+    it('should mount CardContent component',()=>{
+        expect(Wrapper.find('.card-content.clearfix')).to.have.length(1)
+    });
+
+    it('should have style=color:red',()=>{
+       expect(Wrapper.props().style.color).to.equal('red')
+    });
+
+    it('should have children=hello world',()=>{
+        expect(Wrapper.props().children).to.equal('hello world')
+    })
+
+});
