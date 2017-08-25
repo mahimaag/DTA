a5067f232f2f77bfee4b3c5877f3ab97133a6eab
Puja Goyal <puja.goyal@tothenew.com>
2017-08-25 11:47:53 +0530

    changed delete log message

--- client/app/Core/DeleteModal.js
+++ client/app/Core/DeleteModal.js
@@ -9,7 +9,9 @@ class DeleteModal extends Component {
     render(){
         return(
             <div>
-                <h3>Are you sure you want to delete?</h3>
+                <h3>Are you sure you want to delete this log?</h3>
+                <p>Deleted logs cannot be recovered</p>
+                <p>Note this will not delete the collaborator records</p>
                 <TtnButton level="secondary" title="Yes" onClick = {this.props.deleteActivity}/>
                 <TtnButton level="secondary" title="No" onClick = {this.props.close}/>
             </div>
