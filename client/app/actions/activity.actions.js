6d40b48175040445fdf802b8003a0efbc5463985
sourabh <=>
2017-08-25 11:37:22 +0530

    changes made redirect url file

--- client/app/actions/activity.actions.js
+++ client/app/actions/activity.actions.js
@@ -1,8 +1,7 @@
 import { ActivityActions } from './../../constants/actions';
 import {decoratedFetch } from "../config/network.config"
-import fetch from "isomorphic-fetch"
 import { ApiResponseCode } from "../network/constants"
-const AUTHORIZE_URL = "http://newers-world-oauth.qa2.tothenew.net/oauth/authorize?client_id=e6d6a83e-6c7a-11e7-9394-406186be844b";
+
 export const postActivities = (activityLog) => {
     return  (dispatch) => {
         decoratedFetch('/api/activity/employee',{body:activityLog})
@@ -18,9 +17,9 @@ export const postActivities = (activityLog) => {
         })
             .catch(error => {
                 console.log(error)
-            })
-    };
+            })};
 };
+
 export const getActivities = () => {
     return (dispatch) => {
         decoratedFetch('/api/activity/employee',{method:'get'})
@@ -55,5 +54,6 @@ export const deleteActivity = (activityId) => {
             .catch(error => {
                 console.log(error)
             })
+
     }
 };
